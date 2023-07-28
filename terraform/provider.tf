b6da27c88fcda6173be1c15bad5390e1381babc6
Sakshi <89094852+Sakshi-209@users.noreply.github.com>
2023-07-28 18:37:37 +0530

    Add files via upload

---
+++ terraform/provider.tf
@@ -0,0 +1,29 @@
+
+
+terraform {
+
+  required_providers {
+
+    newrelic = {
+
+      source = "newrelic/newrelic"
+
+      version = "3.26.0"
+
+    }
+
+  }
+
+}
+
+ 
+
+provider "newrelic" {
+
+  account_id = 4043634
+
+#   api_key = "**********"   # Usually prefixed with 'NRAK'
+
+  region = "US"  
+
+}
